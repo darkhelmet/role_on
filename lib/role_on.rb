1f27e3d52d5ef159769ba9b4d8ac13483b9537a4
Daniel Huckstep <daniel.huckstep@codebaby.com>
2009-07-30 11:44:22 -0600

    fix named scopes some more...

--- lib/role_on.rb
+++ lib/role_on.rb
@@ -33,8 +33,8 @@ module RoleOn
 
   module RoleOnUserClassMethods
     def helper_for(role,name = role.to_s.pluralize)
-      named_scope(name, lambda { { :conditions => ['role_id = ?', Role[role].id], :joins => :roles } })
-      named_scope("non_#{name}", lambda { { :conditions => [ 'roles.id is ? or roles.id != ?', nil, Role[role].id ], :joins => :roles } })
+      named_scope(name, lambda { { :conditions => ['roles.id = ?', Role[role].id], :joins => :roles } })
+      named_scope("non_#{name}", lambda { { :conditions => [ 'roles.id is ? or roles.id != ?', nil, Role[role].id ], :include => :roles } })
     end
   end
 
